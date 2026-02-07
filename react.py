cd30f8bb648cc452872c7608f3c3e9e77d3c6fab
Celal Okan Uruçay <celalokanurucay@gmail.com>
2026-02-08 02:31:07 +0300

    graph

--- react.py
+++ react.py
@@ -1,9 +1,12 @@
+import os
 from dotenv import load_dotenv  
 from langchain_core.tools import tool
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
 load_dotenv(override=True)
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
 
 @tool
 def triple(num: float) -> float:
@@ -15,4 +18,9 @@ def triple(num: float) -> float:
 
 tools = [TavilySearch(max_results=1), triple]
 
-llm = ChatOpenAI(model="gemini-2.5-flash", temperature=0)
+llm = ChatOpenAI(
+    model="gemini-2.5-flash", 
+    api_key= gemini_api_key,
+    openai_api_base="https://generativelanguage.googleapis.com/v1beta/openai/",
+    temperature=0
+).bind_tools(tools)
